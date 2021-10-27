af1b6922082486b08ce0e015fad9ae0142ad9dd0
kapil <kapil.kumaria@gmail.com>
2021-10-27 22:39:16 +0000

    bastion server working for instances.tf

--- instances.tf
+++ instances.tf
@@ -1,9 +1,13 @@
 resource "aws_instance" "bastionec2" {
-    count = var.env == "dev" ? 1 : 0
+    #count = var.env == "dev" ? 1 : 0
     ami = lookup(var.ec2-ami,var.region)
     instance_type = var.instance-type
     associate_public_ip_address = "true"
-    subnet_id = aws_subnet.subnets.count.index
-    #availability_zone = var.bastionaz
-    security_groups = [aws_security_group.bastionsg.name]
+    #subnet_id = "aws_subnet.subnets[count.index].[0]"
+    subnet_id = data.aws_subnet.pub-subnet1.id
+    security_groups = [aws_security_group.bastionsg.id]
+
+    tags = {
+        Name = "bastion server"
+    }
 }
