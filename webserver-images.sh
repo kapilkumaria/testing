97c3ff388b6bf60225f75c76b2c1f69bfd6b4325
kapil <kapil.kumaria@gmail.com>
2021-10-28 17:09:34 +0000

    modified loadbalancer.tf

--- webserver-images.sh
+++ webserver-images.sh
@@ -17,8 +17,8 @@ service apache2 enable
 mkdir /var/www/html/images
 cd /var/www/html/images
 touch image1 image2 image3
-echo "<html><h1>This is image number 1"</h1></html> > /var/www/html/images/image1
-echo "<html><h1>This is image number 2"</h1></html> > /var/www/html/images/image2
-echo "<html><h1>This is image number 3"</h1></html> > /var/www/html/images/image3
+echo "<html><h1>This is image number 1</h1></html>" > /var/www/html/images/image1
+echo "<html><h1>This is image number 2</h1></html>" > /var/www/html/images/image2
+echo "<html><h1>This is image number 3</h1></html>" > /var/www/html/images/image3
 echo "<html><h1>Testing the Apache Server Successfully from $(hostname -f)</h1></html>" > /var/www/html/index.html
 
--- webserver-logfiles.sh
+++ webserver-logfiles.sh
@@ -18,8 +18,8 @@ service apache2 enable
 mkdir /var/www/html/logfiles
 cd /var/www/html/logfiles
 touch log1 log2 log3
-echo "<html><h1>This is image number 1"</h1></html> > /var/www/html/logfiles/log1
-echo "<html><h1>This is image number 2"</h1></html> > /var/www/html/logfiles/log2
-echo "<html><h1>This is image number 3"</h1></html> > /var/www/html/logfiles/log3
+echo "<html><h1>This is log number 1</h1></html>" > /var/www/html/logfiles/log1
+echo "<html><h1>This is log number 2</h1></html>" > /var/www/html/logfiles/log2
+echo "<html><h1>This is log number 3</h1></html>" > /var/www/html/logfiles/log3
 echo "<html><h1>Testing the Apache Server Successfully from $(hostname -f)</h1></html>" > /var/www/html/index.html
 
