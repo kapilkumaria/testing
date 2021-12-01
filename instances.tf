3f6190d8ee457b490a33bbeaca1d7a333c835fd2
kapil <kapil.kumaria@gmail.com>
2021-12-01 18:41:15 +0000

    fixed launching db servrs in private subnets

--- instances.tf
+++ instances.tf
@@ -56,12 +56,27 @@ resource "aws_instance" "dbserver1" {
     instance_type = var.instance-type
     associate_public_ip_address = "true"
     #subnet_id = "aws_subnet.subnets[count.index].[0]"
-    subnet_id = data.aws_subnet.pub-subnet1.id
+    subnet_id = data.aws_subnet.pub-subnet4.id
     security_groups = [aws_security_group.dbsg.id]
     key_name = var.key-name
     
     tags = {
-        Name = "database server"
+        Name = "database server - 1"
     }
 }    
 
+resource "aws_instance" "dbserver2" {
+    count = var.env-db == "db" ? 1 : 0
+
+    ami = lookup(var.ec2-ami,var.region)
+    instance_type = var.instance-type
+    associate_public_ip_address = "true"
+    #subnet_id = "aws_subnet.subnets[count.index].[0]"
+    subnet_id = data.aws_subnet.pub-subnet5.id
+    security_groups = [aws_security_group.dbsg.id]
+    key_name = var.key-name
+    
+    tags = {
+        Name = "database server - 2"
+    }
+}    
